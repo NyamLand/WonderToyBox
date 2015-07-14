354e12a64c0b4cdf5e9efb7a4bbaf01708e057d7
高鳥義行 <wtpmjgdadgjptw@gmail.com>
2015-07-14 17:56:40 +0900

    ベジェ曲線追加

--- source/GlobalFunction.cpp
+++ source/GlobalFunction.cpp
@@ -229,6 +229,71 @@
 		return	false;
 	}
 
+//----------------------------------------------------------------------------
+//	高次元ベジェ曲線パラメータ取得( 0.0f ~ 1.0f )
+//----------------------------------------------------------------------------
+
+	const static int nPasTgl[12][12] = {
+		{ 1 },
+		{ 1, 1 },
+		{ 1, 2, 1 },
+		{ 1, 3, 3, 1 },
+		{ 1, 4, 6, 4, 1 },
+		{ 1, 5, 10, 10, 5, 1 },
+		{ 1, 6, 15, 20, 15, 6, 1 },
+		{ 1, 7, 21, 35, 35, 21, 7, 1 },
+		{ 1, 8, 28, 56, 70, 56, 28, 8, 1 },
+		{ 1, 9, 36, 84, 126, 126, 84, 36, 9, 1 },
+		{ 1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1 },
+		{ 1, 11, 55, 165, 330, 464, 464, 330, 165, 55, 11, 1 }
+	};
+
+	inline float Get( const float &y1, const float &y2, const float &t, const int &n ){
+		float b = t > 1 ? 1 : (t < 0 ? 0 : t);
+		float a = 1 - b;
+		float ay = 0;
+		float y[4] = { 0, y1, y2, 1 };
+		int m;
+		for (int i = 0; i <= n; i++){
+			m = i == 0 ? 0 : (i == n ? 3 : (i <= n / 2 ? 1 : 2));//yの添え字決定
+			ay += nPasTgl[n][i] * pow(a, n - i)*pow(b, i)*y[m];//※1
+		}
+		return ay;
+	}
+
+	//	ベジェ曲線パラメータ取得（ 0.0f ~ 1.0f で返す ）　
+	float GetBezier( ePrm_t ePrm1, ePrm_t ePrm2, float fRate ){
+		int n = 3;                //n次元指定
+		float y1, y2;
+		switch (ePrm1){
+		case eSlow_Lv5: y1 = 0;                       n = 11; break;//11次元
+		case eSlow_Lv4: y1 = 0;                       n = 9; break;//9次元
+		case eSlow_Lv3: y1 = 0;                       n = 7; break;//7次元
+		case eSlow_Lv2: y1 = 0;                       n = 5; break;//5次元
+		case eSlow_Lv1: y1 = 0;                       n = 3; break;//3次元
+		case eNoAccel: y1 = 0.333333f;               n = 3; break;//直線の場合は3次元中1/3の点
+		case eRapid_Lv1: y1 = 1;                       n = 3; break;//3次元
+		case eRapid_Lv2: y1 = 1;                       n = 5; break;//5次元
+		case eRapid_Lv3: y1 = 1;                       n = 7; break;//7次元
+		case eRapid_Lv4: y1 = 1;                       n = 9; break;//9次元
+		case eRapid_Lv5: y1 = 1;                       n = 11; break;//11次元
+		}
+		switch (ePrm2){
+		case eSlow_Lv5: y2 = 1;                       n = 11; break;//11次元
+		case eSlow_Lv4: y2 = 1;                       n = 9; break;//9次元
+		case eSlow_Lv3: y2 = 1;                       n = 7; break;//7次元
+		case eSlow_Lv2: y2 = 1;                       n = 5; break;//5次元
+		case eSlow_Lv1: y2 = 1;                       n = 3; break;//3次元
+		case eNoAccel: y2 = 0.6666667f;              n = 3; break;//直線の場合は3次元中2/3の点
+		case eRapid_Lv1: y2 = 0;                       n = 3; break;//3次元
+		case eRapid_Lv2: y2 = 0;                       n = 5; break;//5次元
+		case eRapid_Lv3: y2 = 0;                       n = 7; break;//7次元
+		case eRapid_Lv4: y2 = 0;                       n = 9; break;//9次元
+		case eRapid_Lv5: y2 = 0;                       n = 11; break;//11次元
+		}
+		return Get(y1, y2, fRate, n);
+	}
+
 //-------------------------------------------------------------------------
 //	図形描画
 //-------------------------------------------------------------------------
