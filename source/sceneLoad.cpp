0f623f4ac8e669506e3f4db0ccc1743fe8fa912b
ryuki <drgn0722@yahoo.co.jp>
2015-10-20 14:22:09 +0900

    ロード更新とプッシュテスト

--- source/sceneLoad.cpp
+++ source/sceneLoad.cpp
@@ -112,9 +112,10 @@ using namespace std;
 		DrawString( "ロード中", 200, 300 );
 
 		//	pressSpace描画
-		RenderImage(load_anykey, 0, 0, 256, 128, IMAGE_MODE::FLASH);
-		RenderImage(load_anykey, 0, 0, 256, 128, IMAGE_MODE::WAVE);
-
+		if (threadState){
+			RenderImage(load_anykey, 0, 0, 256, 128, IMAGE_MODE::FLASH);
+			RenderImage(load_anykey, 0, 0, 256, 128, IMAGE_MODE::WAVE);
+		}
 
 
 		//デバック用
